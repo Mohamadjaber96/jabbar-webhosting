ffa7176141991ab3fc56c717fc911286450e821f
Wrafofi <ali.guemmi42@gmail.com>
2023-12-29 17:59:05 +0500

    first commit

---
+++ server.js
@@ -0,0 +1,8 @@
+const express = require('express')
+const app = express()
+
+app.get('/', (req, res) => res.send('hope doing well'))
+
+var server = app.listen(process.env.PORT || 4000, () => {
+    console.log("Listening on port " + server.address().port + "...");
+});
